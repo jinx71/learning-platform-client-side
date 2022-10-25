df022dafd3e3b45aa6d2f019d76fe356b8848347
jinx71 <karim.sazed@gmail.com>
2022-10-26 02:12:04 +0600

    Navigation Setup, dark mode enabled.

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Background from '../Component/Root/Background';
+import Navigation from '../Component/Shared/Navigation/Navigation';
 
 const Main = () => {
     return (
-        <Outlet></Outlet>
+        <>
+            <Background>
+                <Navigation></Navigation>
+                <header className="flex flex-col items-center justify-center p-8">
+                    <img className="shadow-lg rounded-full inline-block w-40 h-40 m-8" src={`https://picsum.photos/600?grayscale&random=1`} alt="logo" />
+                    <h1 className="font-bold capitalize text-gray-500 dark:text-gray-400 text-center text-lg pt-4">Try-Not-to-Laugh Challenge</h1>
+                </header>
+                <Outlet></Outlet>
+            </Background>
+        </>
     );
 };
 
