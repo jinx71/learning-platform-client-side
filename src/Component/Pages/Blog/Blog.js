9e50a161400ad4dde644cb8143f1fe3691d87d95
jinx71 <karim.sazed@gmail.com>
2022-10-27 11:10:04 +0600

    faq added

--- src/Component/Pages/Blog/Blog.js
+++ src/Component/Pages/Blog/Blog.js
@@ -7,23 +7,23 @@ const Blog = () => {
     return (
         <div className='container'>
             <div className='border w-1/2 mx-auto'>
-                <h1 className='text-5xl'>what is cors?</h1>
+                <h1 className='text-2xl'>what is cors?</h1>
                 <p class="font-bold">Answer</p>
                 <p>Cross-origin resource sharing is a mechanism that allows restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served. A web page may freely embed cross-origin images, stylesheets, scripts, iframes, and videos. </p>
             </div>
             <div className='border w-1/2 mx-auto'>
-                <h1 className='text-5xl'>Why are you using firebase? What other options do you have to implement authentication?</h1>
+                <h1 className='text-2xl'>Why are you using firebase? What other options do you have to implement authentication?</h1>
                 <p class="font-bold">Answer</p>
                 <p>I am using firebase for authentication & hosting purpose</p>
                 <p>There are several other options for authentication purpose. Like OCTA</p>
             </div>
             <div className='border w-1/2 mx-auto'>
-                <h1 className='text-5xl'>How does the private route work?</h1>
+                <h1 className='text-2xl'>How does the private route work?</h1>
                 <p className='font-bold'>Answer</p>
                 <p>The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in).</p>
             </div>
             <div className='border w-1/2 mx-auto'>
-                <h1 className='text-5xl'>What is Node? How does Node work?</h1>
+                <h1 className='text-2xl'>What is Node? How does Node work?</h1>
                 <p className='font-bold'>Answer</p>
                 <p>Node. js is a JavaScript runtime environment that achieves low latency and high throughput by taking a “non-blocking” approach to serving requests. In other words, Node. js wastes no time or resources on waiting for I/O requests to return.</p>
             </div>
--- src/Component/Pages/Faq/Faq.js
+++ src/Component/Pages/Faq/Faq.js
@@ -4,25 +4,25 @@ const Faq = () => {
     return (
         <div className='container'>
             <div className='border w-1/2 mx-auto'>
-                <h1 className='text-5xl'>What do Ditial Cloud courses include?</h1>
+                <h1 className='text-2xl'>What do Ditial Cloud courses include?</h1>
                 <p class="font-bold">Answer</p>
                 <p>Each digital cloud course is created, owned and managed by the instructors. The foundation of each digital cloud course are its lectures, which can include videos, slides, and text. In addition, instructors can add resources and various types of practice activities, as a way to enhance the learning experience of students.  </p>
             </div>
             <div className='border w-1/2 mx-auto'>
-                <h1 className='text-5xl'>How do I take a digital cloud course?</h1>
+                <h1 className='text-2xl'>How do I take a digital cloud course?</h1>
                 <p class="font-bold">Answer</p>
 
                 <p>Digital cloud courses are entirely on-demand and they can be accessed from several different devices and platforms, including a desktop, laptop, and our mobile app.After you enroll in a course, you can access it by clicking on the course link you will receive in your confirmation email provided you are logged into your digital cloud account. You can also begin the course by logging in and navigating to your My learning page. </p>
             </div>
             <div className='border w-1/2 mx-auto'>
-                <h1 className='text-5xl'>Is there any way to preview a course?</h1>
+                <h1 className='text-2xl'>Is there any way to preview a course?</h1>
                 <p className='font-bold'>Answer</p>
                 <p>Yes! Learn how to preview a course, and review key information about it. This article outlines how you can search digital cloud/'s ever-growing library, and find courses you are interested in taking.
 
                 </p>
             </div>
             <div className='border w-1/2 mx-auto'>
-                <h1 className='text-5xl'>Where can I go for help?
+                <h1 className='text-2xl'>Where can I go for help?
                 </h1>
                 <p className='font-bold'>Answer</p>
                 <p>If you find you have a question about a paid course while you are taking it, you can search for answers to your question in the Q&A or ask the instructor. </p>
